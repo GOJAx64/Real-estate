d9f1d868a06d09886dc927d8ceb4a9cf20524675
GOJAx64 <josealbertogarcia453@gmail.com>
2023-03-23 20:18:31 -0600

    Creating pages for auth (login, register and forgot password)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,11 +2,18 @@
 export const loginForm = (req, res) => {
     res.render('auth/login', {
         authenticated: true,
+        page: 'Login'
     })
 }
 
 export const registerForm = (req, res) => {
     res.render('auth/register', {
-        
+        page: 'Create Account'
+    })
+}
+
+export const forgotPasswordForm = (req, res) => {
+    res.render('auth/forgot-password', {
+        page: 'Restore your password'
     })
 }
